febe01a702d6f9dd0b96da3185ec573876923184
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2021-01-30 10:19:57 +0100

    define / for FRD

--- src/frd.jl
+++ src/frd.jl
@@ -24,17 +24,19 @@ end
 Base.:*(i, ::Type{rad}) = rad(i)
 (::Colon)(start::Union{Hz,rad}, stop::Union{Hz,rad}) = (start, stop)
 
-import Base: +, -, *, length, sqrt, getindex
+import Base: +, -, *, /, length, sqrt, getindex
 FRD(w, s::LTISystem) = FRD(w, freqresp(s, w)[:, 1, 1])
 Base.vec(f::FRD) = f.r
 *(f::FRD, f2)    = FRD(f.w, f.r .* vec(f2))
 +(f::FRD, f2)    = FRD(f.w, f.r .+ vec(f2))
 -(f::FRD, f2)    = FRD(f.w, f.r .- vec(f2))
+/(f::FRD, f2)    = FRD(f.w, f.r ./ vec(f2))
 
 # Below are required for ambiguities
 *(f::FRD, f2::FRD) = FRD(f.w, f.r .* f2.r)
 +(f::FRD, f2::FRD) = FRD(f.w, f.r .+ f2.r)
 -(f::FRD, f2::FRD) = FRD(f.w, f.r .- f2.r)
+/(f::FRD, f2::FRD) = FRD(f.w, f.r ./ f2.r)
 
 *(f::FRD, f2::LTISystem) = *(f, FRD(f.w, f2))
 +(f::FRD, f2::LTISystem) = +(f, FRD(f.w, f2))
