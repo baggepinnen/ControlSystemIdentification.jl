2a17f5176d8790db85c31178beb43bd2e8de88ae
Marius Pille <marius.pille@hu-berlin.de>
2021-03-14 19:16:02 +0100

    change ar definition

--- src/arx.jl
+++ src/arx.jl
@@ -151,7 +151,7 @@ end
 """
     ar(d::AbstractIdData, na; λ=0, estimator=\\, scaleB=false, stochastic=false)
 
-Estimate an AR transfer function (only poles).
+Estimate an AR transfer function `G = 1/A`, the AR process is defined as `A(z⁻¹)y(t) = e(t)`
 
 # Arguments:
 - `d`: iddata
@@ -160,6 +160,48 @@ Estimate an AR transfer function (only poles).
 - `estimator`: e.g. `\\,tls,irls,rtls`
 - `scaleB`: Whether or not to scale the numberator using the variance of the prediction error.
 - `stochastic`: if true, returns a transfer function with uncertain parameters represented by `MonteCarloMeasurements.Particles`.
+
+# Example
+```jldoctest
+julia> N = 10000
+10000
+
+julia> e = [-0.2; zeros(N-1)] # noise e
+10000-element Vector{Float64}:
+[...]
+
+julia> G = tf([1, 0], [1, -0.9], 1) # AR transfer function
+TransferFunction{Discrete{Int64}, ControlSystems.SisoRational{Float64}}
+   1.0z
+----------
+1.0z - 0.9
+
+Sample Time: 1 (seconds)
+Discrete-time transfer function model
+
+julia> y = lsim(G, u, 1:N)[1][:] # Get output of AR transfer function from input noise e
+10000-element Vector{Float64}:
+[...]
+
+julia> Gest = ar(iddata(y), 1) # Estimate AR transfer function from output y
+TransferFunction{Discrete{Float64}, ControlSystems.SisoRational{Float64}}
+          1.0z
+-------------------------
+1.0z - 0.8999999999999998
+
+Sample Time: 1.0 (seconds)
+Discrete-time transfer function model
+
+julia> G ≈ Gest # Test if estimation was correct
+true
+
+julia> eest = lsim(1/Gest, y, 1:N)[1][:] # recover the input noise e from output y and estimated transfer function
+10000-element Vector{Float64}:
+[...]
+
+julia> isapprox(eest, e, atol = eps()) # input noise correct recovert
+true 
+```
 """
 function ar(d::AbstractIdData, na; λ = 0, estimator = \, scaleB = false, stochastic = false)
     noutputs(d) == 1 || throw(ArgumentError("Only 1d time series supported"))
@@ -529,8 +571,8 @@ end
 
 function params2poly(w, na)
     a = [1; -w]
-    b = 1
-    a, b
+    b = [1; zeros(na)]
+    return a, b
 end
 """
 w, a, b = params(G::TransferFunction)
@@ -581,8 +623,14 @@ function ControlSystems.TransferFunction(
 )
     wm, am, bm = params(G)
     na, nb     = length(am), length(bm)
-    if nb == 1 && size(Σ, 1) < length(wm)
-        p = T(N, MvNormal(wm[1:end-1], Σ))
+    if nb != 1 ## AR can now have length(bm) > 1
+        isAR = bm[1] == 1 && all(bm[2:end] .== 0)
+    else
+        isAr = true
+    end
+
+    if isAR && size(Σ, 1) < length(wm)
+        p = T(N, MvNormal(wm[1:end-nb], Σ))
         a, b = params2poly(p, na)
     else
         p = T(N, MvNormal(wm, Σ))
