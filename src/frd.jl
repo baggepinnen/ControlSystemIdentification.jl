2cc17911923bf6e6080f553b5a6d42186978dee5
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2021-05-06 07:10:37 +0200

    add docs to crosscorplot

--- src/frd.jl
+++ src/frd.jl
@@ -260,6 +260,14 @@ end
 
 
 @userplot Crosscorplot
+
+"""
+    crosscorplot(data, [lags])
+
+Plot the cross correlation betweein input and output for `lags` that default to 10% of the length of the dataset on the negative side and 50% on the positive side but no more than 100 on each side.
+"""
+crosscorplot
+
 @recipe function crosscorplot(p::Crosscorplot)
     d = p.args[1]
     N = length(d)
