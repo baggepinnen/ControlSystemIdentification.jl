a82b87051af117539e1931abff9cde2cf8be1d66
Marius <44611200+mapi1@users.noreply.github.com>
2021-08-19 12:20:22 +0200

    Update src/arx.jl
    Co-authored-by: Fredrik Bagge Carlson <baggepinnen@gmail.com>

--- src/arx.jl
+++ src/arx.jl
@@ -120,7 +120,13 @@ end
 
 Fit a transfer Function to data using an ARX model and equation error minimization.
 
-- `nb` and `na` are the number of coefficients of the numerator and denominator polynomials. Input delay can be added via `inputdelay = d`, which corresponds to an additional delay of `z^-1`.  An `inputdelay = 0` results in a direct term. The highest order of the B polynomial is given by `nb + inputdelay - 1`.  `λ > 0` can be provided for L₂ regularization. `estimator` defaults to \\ (least squares), alternatives are `estimator = tls` for total least-squares estimation. `arx(Δt,yn,u,na,nb, estimator=wtls_estimator(y,na,nb)` is potentially more robust in the presence of heavy measurement noise. The number of free parameters is `na+nb` 
+- `nb` and `na` are the number of coefficients of the numerator and denominator polynomials.
+Input delay can be added via `inputdelay = d`, which corresponds to an additional delay of `z^-d`.
+An `inputdelay = 0` results in a direct term.
+The highest order of the B polynomial is given by `nb + inputdelay - 1`.  `λ > 0` can be provided for L₂ regularization.
+`estimator` defaults to \\ (least squares), alternatives are `estimator = tls` for total least-squares estimation. 
+`arx(Δt,yn,u,na,nb, estimator=wtls_estimator(y,na,nb)` is potentially more robust in the presence of
+heavy measurement noise. The number of free parameters is `na+nb` 
 - `stochastic`: if true, returns a transfer function with uncertain parameters represented by `MonteCarloMeasurements.Particles`.
 
 Supports MISO estimation by supplying an iddata with a matrix `u`, with nb = [nb₁, nb₂...] and optional inputdelay = [d₁, d₂...]
