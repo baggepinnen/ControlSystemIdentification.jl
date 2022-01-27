69de73813749081db0b30d44bd3805c4a958f409
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-01-27 07:40:48 +0100

    add initial convex stage to tfest

--- src/arx.jl
+++ src/arx.jl
@@ -574,6 +574,22 @@ function arma(d::AbstractIdData, na, nc; initial_order = 20, estimator = \)
     model
 end
 
+"""
+Freqresp
+"""
+function vconv(w::T, vars::AbstractArray{R}) where {T, R}
+    n = size(vars,1)
+    S = promote_type(T, R)
+    c = zeros(S, 2n - 1)
+    vars = reverse(vars)
+    sv = [1,0,-1,0]
+    for i in 1:n, j in 1:n
+        s = sv[abs(i-j) % 4 + 1]
+        val = (w^(i-1) * w^(j-1) * vars[i]*vars[j])[]
+        @inbounds c[i + j - 1] += s*val
+    end
+    return sum(c)
+end
 
 
 """
@@ -582,6 +598,7 @@ end
         p0,
         link = log ∘ abs;
         freq_weight = sqrt(data.w[1]*data.w[end]),
+        refine = true,
         opt = BFGS(),
         opts = Optim.Options(
             store_trace       = true,
@@ -600,11 +617,22 @@ end
 
 Fit a parametric transfer function to frequency-domain data.
 
+The initial pahse of the optimization solves
+```math
+\\operatorname{minimize}_{B,A}{|| B - A|l|^2||}
+```
+and the second stage (if refine=true) solves 
+```math
+\\operatorname{minimize}_{B,A}{|| \\text{link}\left(\\dfrac{B}{A}\right) - \\text{link}\left(l\right)||}
+```
+(`abs2(link(B/A) - link(l))`)
+
 # Arguments:
 - `data`: An `FRD` onbject with frequency domain data.
 - `p0`: Initial parameter guess. Can be a `NamedTuple` or `ComponentVector` with fields `b,a` specifying numerator and denominator as they appear in the call to `tf`, i.e., `(b = [1.0], a = [1.0,1.0,1.0])`. Can also be an instace of `TransferFunction`.
 - `link`: By default, phase information is discarded in the fitting. To include phase, change to `link = log`.
 - `freq_weight`: Apply weighting with the inverse frequency. The value determines the cutoff frequency before which the weight is constant, after which the weight decreases linearly. Defaults to the geometric mean of the smallest and largest frequency.
+- `refine`: Indicate whether or not a second optimization stage is performed to refine the results of the first.
 - `opt`: The Optim optimizer to use.
 - `opts`: `Optim.Options` controlling the solver options.
 """
@@ -613,6 +641,7 @@ function tfest(
     p0::Union{NamedTuple, ComponentArray},
     link = log ∘ abs;
     freq_weight = sqrt(data.w[2] * data.w[end]),
+    refine = true,
     opt = BFGS(),
     opts = Optim.Options(
         store_trace       = true,
@@ -631,11 +660,12 @@ function tfest(
     # TODO: implement https://epubs.siam.org/doi/pdf/10.1137/140961511 which is the algorithm used in matlab, https://se.mathworks.com/help/ident/ref/tfest.html#bvgwvmc
 
     ladr = @. link(data.r)
+    w, l = data.w, data.r
     if freq_weight > 0
         wv = 1 ./ (data.w .+ freq_weight)
     end
 
-    function loss(p)
+    function loss2(p)
         a, b = p.a, p.b
         G = tf(b, a)
         mag = vec(freqresp(G, data.w))
@@ -646,8 +676,17 @@ function tfest(
         mean(abs2, mag)
     end
 
+    function loss(p)
+        a, b = p.a, p.b
+        mean(eachindex(w)) do i
+            abs2(vconv(w[i], b) - vconv(w[i], a) .* abs2(l[i]))
+        end
+    end
 
     res = Optim.optimize(loss, ComponentVector(p0), opt, opts, autodiff = :forward)
+    if refine
+        res = Optim.optimize(loss2, res.minimizer, opt, opts, autodiff = :forward)
+    end
 
     tf(res.minimizer.b, res.minimizer.a)
 end
