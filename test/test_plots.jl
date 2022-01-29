182e70cfe7bf10728e7d37c5f2a7e8dc791cbff6
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-01-29 15:57:52 +0100

    missing lsim method

--- test/test_plots.jl
+++ test/test_plots.jl
@@ -2,7 +2,7 @@ using ControlSystemIdentification, ControlSystems, Plots
 
 G = c2d(tf(1, [1,1,1]), 0.02)
 u = randn(1, 100)
-res = lsim(G, u)
+res = lsim(G, u, range(0, length=100, step=0.02))
 d = iddata(res)
 
 plot(d)
