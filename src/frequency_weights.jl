99f12b27f100319019cd276173645713c625fec5
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-03-03 15:37:24 +0100

    bugfix due to Hermitian{Diagonal} julia bug

--- src/frequency_weights.jl
+++ src/frequency_weights.jl
@@ -13,23 +13,23 @@ function frequency_weight(system, N)
     n = length(acf)
 
 
-    lastlarge = findlast(x -> abs(x) > sqrt(eps()) * acf[1], acf)
-    if lastlarge === nothing || lastlarge == n
-        lastlarge = n - 1
-    end
-    bands = [0 => Fill(acf[1], n)]
-    for i = 1:lastlarge
-        slice = Fill(acf[i+1], n - i)
-        push!(bands, i => slice)
-        push!(bands, -i => slice)
-    end
-    W = BandedMatrix((bands...,), (n, n))
-
-    # W = similar(acf, n, n)
-    # for di = -n+1:n-1
-    #     W[diagind(W,di)] .= acf[abs(di)+1]
+    # lastlarge = findlast(x -> abs(x) > sqrt(eps()) * acf[1], acf)
+    # if lastlarge === nothing || lastlarge == n
+    #     lastlarge = n - 1
     # end
-    # Symmetric(W)
+    # bands = [0 => Fill(acf[1], n)]
+    # for i = 1:lastlarge
+    #     slice = Fill(acf[i+1], n - i)
+    #     push!(bands, i => slice)
+    #     push!(bands, -i => slice)
+    # end
+    # W = BandedMatrix((bands...,), (n, n))
+
+    W = similar(acf, n, n)
+    for di = -n+1:n-1
+        W[diagind(W,di)] .= acf[abs(di)+1]
+    end
+    Symmetric(W)
 end
 
 weighted_estimator(H::FilterType) = (A, y) -> wls(A, y, frequency_weight(H, size(y, 1)))
