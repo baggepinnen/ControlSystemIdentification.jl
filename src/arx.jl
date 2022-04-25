ea95b2b2a59bfbf3e1ac65cd4036cf5d8187d20b
Fredrik Bagge Carlson <baggepinnen@github.com>
2022-04-25 12:54:59 +0200

    Close #100

--- src/arx.jl
+++ src/arx.jl
@@ -1,12 +1,13 @@
 """
     getARXregressor(y::AbstractVector,u::AbstractVecOrMat, na, nb; inputdelay = ones(Int, size(nb)))
+
 Returns a shortened output signal `y` and a regressor matrix `A` such that the least-squares ARX model estimate of order `na,nb` is `y\\A`
+
 Return a regressor matrix used to fit an ARX model on, e.g., the form
-`A(z)y = B(z)f(u)`
+`A(z)y = B(z)u`
 with output `y` and input `u` where the order of autoregression is `na`,
 the order of input moving average is `nb` and an optional input delay `inputdelay`. Caution, changing the input delay changes the order to `nb + inputdelay - 1`. An `inputdelay = 0` results in a direct term. 
 # Example
-Here we test the model with the Function `f(u) = √(|u|)`
 ```julia
 A     = [1,2*0.7*1,1] # A(z) coeffs
 B     = [10,5] # B(z) coeffs
