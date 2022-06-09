abc96dadeffbdb09104a0d90937a4c21bc5bfbe8
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2022-06-09 14:58:38 +0200

    deterministic model

--- test/test_prediction.jl
+++ test/test_prediction.jl
@@ -2,11 +2,12 @@ using ControlSystemIdentification, ControlSystems
 
 ny = 2
 nu = 3
-nx = 2
+nx = 6
 Ts = 0.1
 N = 300
 
-G = ssrand(ny, nu, nx; Ts)
+G = c2d(tf(1, [1, 1]), Ts) |> ss
+G = [G G G; G G G]
 u = randn(nu, N)
 x0 = randn(nx)
 y, t, x = lsim(G, u; x0)
@@ -19,10 +20,13 @@ e = ControlSystemIdentification.prediction_error(model, d)
 pd = ControlSystemIdentification.predictiondata(d)
 
 pesys = ControlSystemIdentification.prediction_error_filter(model)
-e2 = predict(pesys, pd)
+e2 = predict(pesys, pd, zeros(pesys.nx))
 
 @test mean(abs2, e2 .- e) < 0.01
-@test mean(abs2, (e2 .- e)[:, 4:end]) < 1e-5
+@test mean(abs2, (e2 .- e)[:, nx+1:end]) < 1e-5
+
+# plot((e2 .- e)')
+
 
 residualplot(model, d)
 
