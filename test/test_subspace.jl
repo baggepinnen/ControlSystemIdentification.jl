2e00fc52dc47a5844924304fa77386b4e8f45b57
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-03-02 09:28:03 +0100

    tweak stochastic test

--- test/test_subspace.jl
+++ test/test_subspace.jl
@@ -66,7 +66,7 @@ freqresptest(G, model, tol) = freqresptest(G, model) < tol
         iy, it, ix = impulse(G, 20)
         H = okid(d2, r, 20, p=1)
         # @show norm(iy - permutedims(H, (1, 3, 2)))
-        @test norm(iy - permutedims(H, (1, 3, 2))) < 1e-2
+        @test norm(iy - permutedims(H, (1, 3, 2))) < 2e-2
         # plot([iy vec(H)])
 
         H = okid(d2, r, 20, p = 20)
