434a8ed779e415a385a7745296422ca64a8f09fc
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-09-01 13:12:36 +0200

    add tutorial on delay estimation

--- docs/make.jl
+++ docs/make.jl
@@ -39,6 +39,7 @@ makedocs(
                   "Temperature control" => "examples/temp.md",
                   "Identification in closed loop" => "examples/closed_loop_id.md",
                   "Identification of unstable systems" => "examples/unstable_systems.md",
+                  "Delay estimation" => "examples/delayest.md",
                   "Ball and beam" => "examples/ballandbeam.md",
                   "Flexible robot arm" => "examples/flexible_robot.md",
                   "Glass furnace" => "examples/glass_furnace.md",
